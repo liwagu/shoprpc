14e9d9a32cf3ea5367b122c4b2d2741ea1c3ca4c
root <root@asus>
2024-05-22 20:56:09 +0800

    运行商品服务

--- rpc/item/handler.go
+++ rpc/item/handler.go
@@ -10,6 +10,10 @@ type ItemServiceImpl struct{}
 
 // GetItem implements the ItemServiceImpl interface.
 func (s *ItemServiceImpl) GetItem(ctx context.Context, req *item.GetItemReq) (resp *item.GetItemResp, err error) {
-	// TODO: Your code here...
+	resp = item.NewGetItemResp()
+	resp.Item = item.NewItem()
+	resp.Item.Id = req.GetId()
+	resp.Item.Title = "Kitex"
+	resp.Item.Description = "Kitex is an excellent framework!"
 	return
 }
