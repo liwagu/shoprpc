b55fdefeaddd561e01499e5c9ea964b282827439
root <root@asus>
2024-05-22 21:29:01 +0800

    运行库存服务

--- rpc/stock/main.go
+++ rpc/stock/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"log"
+	"net"
 	stock "shoprpc/kitex_gen/example/shop/stock/stockservice"
+	"github.com/cloudwego/kitex/server"
 )
 
 func main() {
-	svr := stock.NewServer(new(StockServiceImpl))
+    addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8890")
+    svr := stock.NewServer(new(StockServiceImpl), server.WithServiceAddr(addr))
 
-	err := svr.Run()
+    err := svr.Run()
 
-	if err != nil {
-		log.Println(err.Error())
-	}
+    if err != nil {
+       log.Println(err.Error())
+    }
 }
